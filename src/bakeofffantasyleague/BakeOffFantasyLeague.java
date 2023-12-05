51c3b3340966df67288cb4f396ab4cfbb60b4545
Mateus Manhani <58527872+mateusmanhani@users.noreply.github.com>
2023-12-05 20:28:53 +0000

    Player Class

--- src/bakeofffantasyleague/BakeOffFantasyLeague.java
+++ src/bakeofffantasyleague/BakeOffFantasyLeague.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package bakeofffantasyleague;
 
 /**
  *
- * @author Mateus
+ * @author Mateus Manhani
  */
 public class BakeOffFantasyLeague {
 
--- src/bakeofffantasyleague/Player.java
+++ src/bakeofffantasyleague/Player.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package bakeofffantasyleague;
 
 /**
  *
- * @author Mateus
+ * @author Mateus Manhani
  */
 public class Player {
     
+    public Player(String name,){
+    }
 }
